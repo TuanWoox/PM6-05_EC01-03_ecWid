3bce9206ee604a4e3ecb23e9961f3e9f4034a11b
TuanWoox <z1venttt@gmail.com>
2024-10-08 14:11:42 +0700

    first

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3030;
+const path = require('path');
+const ejsMate = require('ejs-mate')
+
+app.set('view engine', 'ejs')
+//This one right here is app.engine used for layouts, partials, block
+app.engine('ejs', ejsMate);
+//This one is for req.render 
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/',(req,res) => {
+    res.render('website/index')
+})
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.listen(PORT,() => {
+    console.log('Serving on POST 3030');
+})
